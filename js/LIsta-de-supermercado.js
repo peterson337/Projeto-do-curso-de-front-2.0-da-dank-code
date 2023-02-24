709679d4deeb6f07ca8a79cc4d0af7c54b3b3971
Peterson Rodrigues dos Santos <petersonpepi16@gmail.com>
2023-02-23 22:46:42 -0200

    Fazendo o projeto

---
+++ js/LIsta-de-supermercado.js
@@ -0,0 +1,44 @@
+var items = [];
+
+document.querySelector("input[type=submit]")
+.addEventListener("click",() => {
+    let nomeProduto = document.querySelector('input[name=nome_produto]')
+    let precoProduto = document.querySelector("input[name=price]")
+    // alert(nomeProduto);
+    // alert(precoProduto);
+    items.push({
+        nome: nomeProduto.value,
+        valor: precoProduto.value
+    });
+
+    // <div class="lista-produto-single">
+//    <h3>RedBull</h3>
+//     <h3 class="prince-produto"><span>R$20,00</span></h3>
+//     </div>
+
+    let listaProdutos = document.querySelector('.lista-produtos');
+    let soma = 0;
+    listaProdutos.innerHTML="";
+    items.map(function(val){
+        soma+=parseFloat(val.valor);
+        listaProdutos.innerHTML+=`
+        <div class="lista-produto-single">
+        <h3>`+val.nome+`</h3>
+        <h3 class="prince-produto"><span>`+val.valor+`</span></h3>
+        </div>
+        `;
+    })
+    soma = soma.toFixed(2);
+    nomeProduto.value="";
+    precoProduto.value="";
+
+    let elementoSoma = document.querySelector(".soma-produto h1");
+    elementoSoma.innerHTML = "R$"+soma;
+});
+
+document.querySelector("button[name=limpar]")
+.addEventListener("click",() => {
+    items = [];
+    document.querySelector('.lista-produtos').innerHTML="";
+    document.querySelector(".soma-produto h1").innerHTML="R$0";
+})
